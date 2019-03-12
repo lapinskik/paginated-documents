f6d32bec76d07856171a2c3ea32294fb77c94b69
root <root@localhost.localdomain>
2019-03-12 14:37:35 +0100

    Deface works now, we have plugin that make using back_url possible.

--- app/overrides/documents/_form.rb
+++ app/overrides/documents/_form.rb
@@ -1,5 +1,4 @@
 Deface::Override.new(:virtual_path => "documents/_form",
                      :name => "change_documents_form",
-                     :insert_after => "#document_title",
-                     :text => "<%= hidden_field_tag 'back_url', params[:back_url], :id => nil if params[:back_url].present? %>
-<%= hidden_field_tag 'continue', params[:continue], :id => nil if params[:continue].present? %>")
+                     :insert_after => "erb[loud]:contains('f.text_field')",
+                     :text => "<%= hidden_field_tag 'back_url', params[:back_url], :id => nil if params[:back_url].present? %><%= hidden_field_tag 'continue', params[:continue], :id => nil if params[:continue].present? %>")
