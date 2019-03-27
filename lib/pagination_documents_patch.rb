2379782c9abbe092a1873c3ec8bd27db7de0cfe3
root <root@dev084.dir.pl>
2019-03-27 22:12:01 +0100

    pagination works, and makes rendering documents faster

--- lib/pagination_documents_patch.rb
+++ lib/pagination_documents_patch.rb
@@ -5,35 +5,33 @@ module DocumentsControllerPatch
         documents = documents.where(id: CustomValue.where(custom_field_id: CustomField.where(id: search_params['cf-id']).ids, value: search_params['cf-value']).map(&:customized_id).uniq)
       end
 
-      def index
-        @custom_fields = CustomField.where(type: 'DocumentCustomField')
-        Rails.logger.error params[:sort_by]
-        @sort_by = (%w(category date title author).include?(params[:sort_by]) || (params.has_key?(:sort_by) && params[:sort_by].start_with?('cf-') && params[:sort_by].split('cf-')[1].to_i.in?(@custom_fields.ids) )) ? params[:sort_by] : 'category'
-        documents = @project.documents
-        documents = formPagination(documents)
-        documents.includes(:attachments, :category).to_a
-        if @sort_by.eql? 'date'
-          @grouped = documents.group_by {|d| d.updated_on.to_date }
-        elsif @sort_by.eql? 'title'
-          @grouped = documents.group_by {|d| d.title.first.upcase}
-        elsif @sort_by.eql? 'author'
-          @grouped = documents.select{|d| d.attachments.any?}.group_by {|d| d.attachments.last.author}
-        elsif @sort_by.start_with?("cf-")
-          cf_num = @sort_by.split('cf-')[1].to_i
-          @grouped = documents.group_by {|d| d.custom_field_value(cf_num)}
-          # @grouped = documents.where(id: CustomValue.where(custom_field_id: CustomField.where(id: ).ids).ids).group_by {|d| d.custom_field_values}
-        else
-          @grouped = documents.group_by(&:category)
-        end
-        @document = @project.documents.build
-        render :layout => false if request.xhr?
+      def prepare_document_custom_field_values(documents_ids)
+        document_custom_fields = CustomField.where(type: 'DocumentCustomField').ids
+        document_custom_fields_values = CustomValue.where(custom_field_id: document_custom_fields, customized_id: documents_ids).to_a
+        @document_custom_fields_values_hash = document_custom_fields_values.to_a.group_by {|x| x['customized_id']}
       end
 
-      def formPagination(entries)
-    		@entry_count = entries.count
-            setLimitAndOffset()
-    		@document = entries.order(:created_on).limit(@limit).offset(@offset)
-    	end
+      def index
+        @custom_fields = CustomField.where(type: 'DocumentCustomField')
+        @sort_by = (%w(category date title author).include?(params[:sort_by]) || (params.has_key?(:sort_by) && params[:sort_by].start_with?('cf-') && params[:sort_by].split('cf-')[1].to_i.in?(@custom_fields.ids) )) ? params[:sort_by] : 'category'
+        @entry_count = @project.documents.includes(:attachments, :category).count
+        setLimitAndOffset
+        if @sort_by.eql? 'date'
+          @sorted = @project.documents.includes(:attachments, :category).order('attachments.created_on').offset(@offset).limit(@limit).to_a
+        elsif @sort_by.eql? 'title'
+          @sorted = @project.documents.includes(:attachments, :category).order(:title).offset(@offset).limit(@limit).to_a
+        elsif @sort_by.eql? 'author'
+          @sorted = @project.documents.includes(:attachments, :category).order('attachments.author_id').offset(@offset).limit(@limit).to_a
+        elsif @sort_by.start_with?("cf-")
+          cf_num = @sort_by.split('cf-')[1].to_i
+          @sorted = @project.documents.joins("LEFT OUTER JOIN custom_values ON custom_values.customized_id = documents.id AND custom_values.custom_field_id = "+cf_num.to_s).order('custom_values.value').offset(@offset).limit(@limit).to_a
+        else
+          @sorted = @project.documents.includes(:attachments, :category).order(:category_id).offset(@offset).limit(@limit).to_a
+        end
+        @document = @project.documents.build
+        prepare_document_custom_field_values(@sorted.map{|x| x['id']})
+        render :layout => false if request.xhr?
+      end
 
       def setLimitAndOffset
   			@entry_pages = Paginator.new @entry_count, per_page_option, params['page']
